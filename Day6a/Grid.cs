a7164ec3b12ca5f06a9117b5066558796dcca8b9
Barry Wallis <barry_wallis@acm.org>
2026-02-15 08:23:49 -0600

    Upgrade all C# projects to language version preview
    - Updated LangVersion to 'preview' in all 20 projects (11 app + 9 test projects)
    
    - Fixed syntax error in Day5b/Program.cs (replaced invalid 'with' usage)
    
    - All 758 tests passing successfully
    
    - Build successful across all projects

---
+++ Day6a/Grid.cs
@@ -0,0 +1,16 @@
+﻿internal enum Operation
+{
+    Add,
+    Multiply
+}
+
+internal class Grid
+{
+    public Grid(TextReader input) => throw new NotImplementedException();
+
+    public int TotalColumns { get; internal set; }
+    public int TotalRows { get; internal set; }
+
+    internal IEnumerable<int> Column(int column) => throw new NotImplementedException();
+    internal Operation Operation(int column) => throw new NotImplementedException();
+}
---
+++ Day6a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+Grid grid = new(Console.In);
+
+BigInteger total = 0;
+for(int column = 0; column < grid.TotalColumns; column++)
+{
+    switch (grid.Operation(column))
+    {
+        case Operation.Add:
+            total += grid.Column(column).Aggregate(0, (accumulator, x) => accumulator + x);
+            break;
+        case Operation.Multiply:
+            total += grid.Column(column).Aggregate(1, (accumulator, x) => accumulator * x);
+            break;
+    }
+}
+
+Console.WriteLine(total.ToString());
